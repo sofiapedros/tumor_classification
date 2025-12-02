bbe9d3eaea4a37e5e87c8e10d973b1f7ac3e9e47
sofiapedros <202108146@alu.comillas.edu>
2025-12-02 15:30:16 +0100

    Allow monitoring

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import torch
 import torch.nn as nn
 import joblib
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 import numpy as np
 
 device = torch.device("cpu")
+PREDICTION_COUNTER = Counter(
+    'tumor_prediction_count', 
+    'contador de predicciones por tipo de tumor',
+    ['tumor'])
 
 try:
     torch_load_orig = torch.load
@@ -24,6 +29,10 @@ except FileNotFoundError:
 
 app = Flask(__name__)
 
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
 @app.route("/predict", methods=["POST"])
 def predict():
     if model is None:
@@ -40,9 +49,12 @@ def predict():
         with torch.no_grad():
             outputs = model(features_tensor)
             predicted_class = torch.argmax(outputs, dim=1).item()
+        tumor_map = {0: "bening", 1:'malignant', 2: 'normal'}
 
+        predicted_tumor = tumor_map.get(predicted_class, 'unknown')
+        PREDICTION_COUNTER.labels(tumor=predicted_tumor).inc()
         # Devolver la predicción
-        return jsonify({"prediction": int(predicted_class)})
+        return jsonify({"prediction": int(predicted_class), 'tumor': predicted_tumor})
     
     except Exception as e:
         return jsonify({"error": str(e)}), 400
